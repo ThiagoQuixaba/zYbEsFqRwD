c93d432d082537b247d631b28610dac1f16d0adc
Thiago-Quixaba <141958972+ThiagoQuixaba@users.noreply.github.com>
2024-05-24 10:33:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ function verificarSenha() {
     const senhaDigitada = document.getElementById('senhaInput').value;
 
     if (senhaDigitada === senhaCorreta) {
-        alert('Hxhxjg t Cpjtaq - R7L8R5L2R9L3R1L4R6')
+        alert('Hxhxjg t Cpjtaq - R5L2R9L3R1L4R6')
     }
 }
