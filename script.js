faf0a006a4044dc88d1c8ca37bc512f9553e7941
Thiago-Quixaba <141958972+ThiagoQuixaba@users.noreply.github.com>
2024-05-23 22:43:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function verificarSenha() {
+    const senhaCorreta = "xU3@p9v$KmLW";
+    const senhaDigitada = document.getElementById('senhaInput').value;
+
+    if (senhaDigitada === senhaCorreta) {
+        alert('Hxhxjg t Cpjtaq - R7L8R5L2R9L3R1L4R6')
+    }
+}
