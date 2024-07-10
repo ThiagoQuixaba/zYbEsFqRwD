f6bf0f7a8dbd69e70b31d5335b2855473363a921
Thiago-Quixaba <141958972+ThiagoQuixaba@users.noreply.github.com>
2024-07-09 22:34:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function verificarSenha() {
     const senhaCorreta = "bpyeml kcyb";
     const senhaDigitada = document.getElementById('senhaInput').value;
 
-    if (senhaDigitada === senhaCorreta) {
+    if (senhaDigitada === "bpyeml kcyb") {
         alert('0, 1, 16, 23, 5, 17, 22')
     }
 }
