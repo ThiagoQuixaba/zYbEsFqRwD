afae0956dd24403f34419a9a7a05256649a17c79
Thiago-Quixaba <141958972+ThiagoQuixaba@users.noreply.github.com>
2024-07-09 22:46:41 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function verificarSenha() {
-    const senhaCorreta = "bpyeml kcyb";
     const senhaDigitada = document.getElementById('senhaInput').value;
 
-    if (senhaDigitada === "bpyeml kcyb") {
+    if (senhaDigitada === "Start") {
         alert('0, 1, 16, 23, 5, 17, 22')
+    } else if (senhaDigitada === "bpyeml kcyb"){
+        
     }
 }
